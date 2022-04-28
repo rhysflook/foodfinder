001af4816022654efbc434592a00e7994b9c3aea
Rhys Flook <rhys.flook@googlemail.com>
2022-04-28 14:48:52 +0900

    make start to favourites post end point

--- backend/addToFavourites.php
+++ backend/addToFavourites.php
@@ -1,4 +1,11 @@
 <?php 
-
+$dsn = "mysql:host=192.168.40.64;dbname=b202213;charset=utf8";
+$u = "rhys";
+$p = "pass";
+$pdo = new PDO( $dsn, $u, $p );
+$sql = "INSERT kiniiri (user_id, google_id) VALUES :user_id, :google_id";
+$st = $pdo->prepare($sql);
+$st->bindValue(":user_id", $_POST['user_id'], PDO::PARAM_INT);
+$st->bindValue(":google_id", $_POST["google_id"], PDO::PARAM_INT);
 
 ?>
